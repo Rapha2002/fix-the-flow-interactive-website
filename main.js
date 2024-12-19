24506afa350b5ae1f42651b17f583d471a976960
Rapha2002 <rapha.koekkoek@gmail.com>
2024-12-19 11:26:59 +0100

    Header fixes #7

--- main.js
+++ main.js
@@ -1,32 +1,19 @@
-
-
-function togglePopup(){
-    document.getElementById("popup-1").classList.toggle("active");
-}
-
-
-document.getElementById('openPopup').addEventListener('click', (event) => {
+document.querySelector('.openPopup').addEventListener('click', (event) => {
     console.log('Clicked')
     document.getElementById('popup-1').classList.toggle('active')
     event.stopPropagation();
 })
 
-document.getElementById('closePopup').addEventListener('click', (event) => {
-    document.getElementById('popup-1').classList.toggle('active')
+document.querySelector('.closePopup').addEventListener('click', (event) => {
+    document.getElementById('popup-1').classList.toggle('active');
     event.stopPropagation();
 })
 
-document.addEventListener('keydown', function (event) {
+document.querySelector('keydown', function (event) {
     if (document.getElementById("popup-1").className.includes('active')) {
         if (event.key === "Escape") {
             document.getElementById("popup-1").classList.toggle("active");
         }
     }
-});
-document.addEventListener('click', function (event) {
-
-    if (document.getElementById("popup-1").className.includes('active')) {
-        document.getElementById("popup-1").classList.toggle("active");
-    }
-});
+})
 
